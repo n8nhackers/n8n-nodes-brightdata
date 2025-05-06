353879fd11f10c77ea0d07892123871aa0c3fa44
Miquel Colomer <mcolomer@gmail.com>
2025-05-06 10:53:48 +0200

    Comment new methods

--- nodes/BrightData/MarketplaceDatasetDescription.ts
+++ nodes/BrightData/MarketplaceDatasetDescription.ts
@@ -13,87 +13,87 @@ export const marketplaceDatasetOperations: INodeProperties[] = [
 			},
 		},
 		options: [
-			{
-				name: 'Deliver Snapshot',
-				value: 'deliverSnapshot',
-				action: 'Deliver the dataset snapshot',
-				routing: {
-					request: {
-						method: 'POST',
-						url: '/datasets/snapshots/{{id}}/deliver',
-						body: {
-							deliver: '={{$parameter["deliver"]}}',
-							compress: '={{$parameter["compress"] || false}}',
-						},
-					},
-				},
-			},
-			{
-				name: 'Filter Dataset',
-				value: 'filterDataset',
-				action: 'Create a dataset snapshot based on a provided filter',
-				routing: {
-					request: {
-						method: 'POST',
-						url: '/datasets/filter',
-						body: {
-							records_limit: '={{$parameter["records_limit"]}}',
-							filter: '={{$parameter["filter"]}}',
-							dataset_id: '={{$parameter["dataset_id"]}}',
-						}
-					},
-				},
-			},
-			{
-				name: 'Get Dataset Metadata',
-				value: 'getDatasetMetadata',
-				action: 'Retrieve detailed metadata for a specific dataset',
-				routing: {
-					request: {
-						method: 'GET',
-						url: '/datasets/{{$parameter["dataset_id"]}}/metadata',
-					},
-				},
-			},
-			{
-				name: 'Get Snapshot Content',
-				value: 'getSnapshotContent',
-				action: 'Get dataset snapshot content',
-				routing: {
-					request: {
-						method: 'GET',
-						url: '/datasets/snapshots/{{snapshot_id}}/download',
-						qs: {
-							format: '={{$parameter["format"]}}',
-							compress: '={{$parameter["compress"] || false}}',
-							batch_size: '={{$parameter["batch_size"]}}',
-							part: '={{$parameter["part"]}}',
-						},
-					},
-				},
-			},
-			{
-				name: 'Get Snapshot Metadata',
-				value: 'getSnapshotMetadata',
-				action: 'Get dataset snapshot metadata',
-				routing: {
-					request: {
-						method: 'GET',
-						url: '/datasets/snapshots/{{$parameter["snapshot_id"]}}/metadata',
-					},
-				},
-			},
-			{
-				name: 'Get Snapshot Parts',
-				value: 'getSnapshotParts',
-				action: 'Get dataset snapshot delivery parts',
-				routing: {
-					request: {
-						method: 'GET',
-						url: '/datasets/snapshots/{{$parameter["snapshot_id"]}}/parts',
-					},
-				},
-			},
+			// {
+			// 	name: 'Deliver Snapshot',
+			// 	value: 'deliverSnapshot',
+			// 	action: 'Deliver the dataset snapshot',
+			// 	routing: {
+			// 		request: {
+			// 			method: 'POST',
+			// 			url: '/datasets/snapshots/{{id}}/deliver',
+			// 			body: {
+			// 				deliver: '={{$parameter["deliver"]}}',
+			// 				compress: '={{$parameter["compress"] || false}}',
+			// 			},
+			// 		},
+			// 	},
+			// },
+			// {
+			// 	name: 'Filter Dataset',
+			// 	value: 'filterDataset',
+			// 	action: 'Create a dataset snapshot based on a provided filter',
+			// 	routing: {
+			// 		request: {
+			// 			method: 'POST',
+			// 			url: '/datasets/filter',
+			// 			body: {
+			// 				records_limit: '={{$parameter["records_limit"]}}',
+			// 				filter: '={{$parameter["filter"]}}',
+			// 				dataset_id: '={{$parameter["dataset_id"]}}',
+			// 			}
+			// 		},
+			// 	},
+			// },
+			// {
+			// 	name: 'Get Dataset Metadata',
+			// 	value: 'getDatasetMetadata',
+			// 	action: 'Retrieve detailed metadata for a specific dataset',
+			// 	routing: {
+			// 		request: {
+			// 			method: 'GET',
+			// 			url: '/datasets/{{$parameter["dataset_id"]}}/metadata',
+			// 		},
+			// 	},
+			// },
+			// {
+			// 	name: 'Get Snapshot Content',
+			// 	value: 'getSnapshotContent',
+			// 	action: 'Get dataset snapshot content',
+			// 	routing: {
+			// 		request: {
+			// 			method: 'GET',
+			// 			url: '/datasets/snapshots/{{snapshot_id}}/download',
+			// 			qs: {
+			// 				format: '={{$parameter["format"]}}',
+			// 				compress: '={{$parameter["compress"] || false}}',
+			// 				batch_size: '={{$parameter["batch_size"]}}',
+			// 				part: '={{$parameter["part"]}}',
+			// 			},
+			// 		},
+			// 	},
+			// },
+			// {
+			// 	name: 'Get Snapshot Metadata',
+			// 	value: 'getSnapshotMetadata',
+			// 	action: 'Get dataset snapshot metadata',
+			// 	routing: {
+			// 		request: {
+			// 			method: 'GET',
+			// 			url: '/datasets/snapshots/{{$parameter["snapshot_id"]}}/metadata',
+			// 		},
+			// 	},
+			// },
+			// {
+			// 	name: 'Get Snapshot Parts',
+			// 	value: 'getSnapshotParts',
+			// 	action: 'Get dataset snapshot delivery parts',
+			// 	routing: {
+			// 		request: {
+			// 			method: 'GET',
+			// 			url: '/datasets/snapshots/{{$parameter["snapshot_id"]}}/parts',
+			// 		},
+			// 	},
+			// },
 			{
 				name: 'List Datasets',
 				value: 'listDatasets',
@@ -141,85 +141,85 @@ const marketplaceDatasetParameters: INodeProperties[] = [
 			},
 		},
 	},
-	{
-		displayName: 'Snapshot ID',
-		name: 'id',
-		type: 'string',
-		default: '',
-		displayOptions: {
-			show: {
-				operation: ['getSnapshotMetadata', 'getSnapshotParts', 'getSnapshotContent', 'deliverSnapshot'],
-			},
-		},
-		required: true,
-		description: 'The ID of the snapshot to operate on',
-	},
+	// {
+	// 	displayName: 'Snapshot ID',
+	// 	name: 'id',
+	// 	type: 'string',
+	// 	default: '',
+	// 	displayOptions: {
+	// 		show: {
+	// 			operation: ['getSnapshotMetadata', 'getSnapshotParts', 'getSnapshotContent', 'deliverSnapshot'],
+	// 		},
+	// 	},
+	// 	required: true,
+	// 	description: 'The ID of the snapshot to operate on',
+	// },
 
-	{
-		displayName: 'Records Limit',
-		name: 'records_limit',
-		type: 'number',
-		default: 100,
-		displayOptions: {
-			show: {
-				operation: ['filterDataset'],
-			},
-		},
-		description: 'The maximum number of records to include in the snapshot'
+	// {
+	// 	displayName: 'Records Limit',
+	// 	name: 'records_limit',
+	// 	type: 'number',
+	// 	default: 100,
+	// 	displayOptions: {
+	// 		show: {
+	// 			operation: ['filterDataset'],
+	// 		},
+	// 	},
+	// 	description: 'The maximum number of records to include in the snapshot'
 
-	},
+	// },
 
-	{
-		displayName: 'Deliver Type',
-		name: 'deliver_type',
-		type: 'options',
-		options: [
-			{
-				name: 'Aliyun Object Storage Service',
-				value: 'ali_oss',
-			},
-			{
-				name: 'Amazon S3',
-				value: 's3',
-			},
-			{
-				name: 'Google Cloud PubSub',
-				value: 'pubsub',
-			},
-			{
-				name: 'Google Cloud Storage',
-				value: 'gcs',
-			},
-			{
-				name: 'Microsoft Azure',
-				value: 'azure',
-			},
-			{
-				name: 'SFTP',
-				value: 'sftp',
-			},
-			{
-				name: 'Snowflake',
-				value: 'snowflake',
-			},
-			{
-				name: 'Webhook',
-				value: 'webhook',
-			},
-		],
-		default: 'webhook',
-		displayOptions: {
-			show: {
-				operation: ['deliverSnapshot'],
-			},
-		},
-		routing: {
-			send: {
-				type: 'body',
-				property: 'deliver.type'
-			}
-		}
-	},
+	// {
+	// 	displayName: 'Deliver Type',
+	// 	name: 'deliver_type',
+	// 	type: 'options',
+	// 	options: [
+	// 		{
+	// 			name: 'Aliyun Object Storage Service',
+	// 			value: 'ali_oss',
+	// 		},
+	// 		{
+	// 			name: 'Amazon S3',
+	// 			value: 's3',
+	// 		},
+	// 		{
+	// 			name: 'Google Cloud PubSub',
+	// 			value: 'pubsub',
+	// 		},
+	// 		{
+	// 			name: 'Google Cloud Storage',
+	// 			value: 'gcs',
+	// 		},
+	// 		{
+	// 			name: 'Microsoft Azure',
+	// 			value: 'azure',
+	// 		},
+	// 		{
+	// 			name: 'SFTP',
+	// 			value: 'sftp',
+	// 		},
+	// 		{
+	// 			name: 'Snowflake',
+	// 			value: 'snowflake',
+	// 		},
+	// 		{
+	// 			name: 'Webhook',
+	// 			value: 'webhook',
+	// 		},
+	// 	],
+	// 	default: 'webhook',
+	// 	displayOptions: {
+	// 		show: {
+	// 			operation: ['deliverSnapshot'],
+	// 		},
+	// 	},
+	// 	routing: {
+	// 		send: {
+	// 			type: 'body',
+	// 			property: 'deliver.type'
+	// 		}
+	// 	}
+	// },
 
 	// {
 	// 	displayName: 'Webhook Parameters',
@@ -1415,116 +1415,116 @@ const marketplaceDatasetParameters: INodeProperties[] = [
 	// },
 
 	//properties for snapshopContent
-	{
-		displayName: 'Format',
-		name: 'format',
-		type: 'options',
-		options: [
-			{
-				name: 'JSON',
-				value: 'json',
-			},
-			{
-				name: 'JSONL',
-				value: 'jsonl',
-			},
-			{
-				name: 'CSV',
-				value: 'csv',
-			},
-		],
-		default: 'json',
-		description: 'Format of the response. Available options: JSON, JSONL, CSV.',
-		displayOptions: {
-			show: {
-				operation: ['getSnapshotContent'],
-			},
-		},
-		routing: {
-			request: {
-				qs: {
-					format: '={{$parameter["format"]}}',
-				},
-			},
-		},
-	},
-	{
-		displayName: 'Compress',
-		name: 'compress',
-		type: 'boolean',
-		default: false,
-		description: 'Whether compress the response in gzip format',
-		displayOptions: {
-			show: {
-				operation: ['getSnapshotContent'],
-			},
-		},
-		routing: {
-			request: {
-				qs: {
-					compress: '={{$parameter["compress"]}}',
-				},
-			}
-		},
-	},
-	{
-		displayName: 'Batch Size',
-		name: 'batch_size',
-		type: 'number',
-		default: 100,
-		description: 'Number of records to include in each response batch',
-		displayOptions: {
-			show: {
-				operation: ['getSnapshotContent'],
-			},
-		},
-		routing: {
-			request: {
-				qs: {
-					batch_size: '={{$parameter["batch_size"]}}',
-				},
-			}
-		},
-	},
-	{
-		displayName: 'Part',
-		name: 'part',
-		type: 'number',
-		default: 1,
-		description: 'Number of batch to return. The numbering starts from 1.',
-		displayOptions: {
-			show: {
-				operation: ['getSnapshotContent'],
-			},
-		},
-		routing: {
-			request: {
-				qs: {
-					part: '={{$parameter["part"]}}',
-				},
-			}
-		},
-	},
+	// {
+	// 	displayName: 'Format',
+	// 	name: 'format',
+	// 	type: 'options',
+	// 	options: [
+	// 		{
+	// 			name: 'JSON',
+	// 			value: 'json',
+	// 		},
+	// 		{
+	// 			name: 'JSONL',
+	// 			value: 'jsonl',
+	// 		},
+	// 		{
+	// 			name: 'CSV',
+	// 			value: 'csv',
+	// 		},
+	// 	],
+	// 	default: 'json',
+	// 	description: 'Format of the response. Available options: JSON, JSONL, CSV.',
+	// 	displayOptions: {
+	// 		show: {
+	// 			operation: ['getSnapshotContent'],
+	// 		},
+	// 	},
+	// 	routing: {
+	// 		request: {
+	// 			qs: {
+	// 				format: '={{$parameter["format"]}}',
+	// 			},
+	// 		},
+	// 	},
+	// },
+	// {
+	// 	displayName: 'Compress',
+	// 	name: 'compress',
+	// 	type: 'boolean',
+	// 	default: false,
+	// 	description: 'Whether compress the response in gzip format',
+	// 	displayOptions: {
+	// 		show: {
+	// 			operation: ['getSnapshotContent'],
+	// 		},
+	// 	},
+	// 	routing: {
+	// 		request: {
+	// 			qs: {
+	// 				compress: '={{$parameter["compress"]}}',
+	// 			},
+	// 		}
+	// 	},
+	// },
+	// {
+	// 	displayName: 'Batch Size',
+	// 	name: 'batch_size',
+	// 	type: 'number',
+	// 	default: 100,
+	// 	description: 'Number of records to include in each response batch',
+	// 	displayOptions: {
+	// 		show: {
+	// 			operation: ['getSnapshotContent'],
+	// 		},
+	// 	},
+	// 	routing: {
+	// 		request: {
+	// 			qs: {
+	// 				batch_size: '={{$parameter["batch_size"]}}',
+	// 			},
+	// 		}
+	// 	},
+	// },
+	// {
+	// 	displayName: 'Part',
+	// 	name: 'part',
+	// 	type: 'number',
+	// 	default: 1,
+	// 	description: 'Number of batch to return. The numbering starts from 1.',
+	// 	displayOptions: {
+	// 		show: {
+	// 			operation: ['getSnapshotContent'],
+	// 		},
+	// 	},
+	// 	routing: {
+	// 		request: {
+	// 			qs: {
+	// 				part: '={{$parameter["part"]}}',
+	// 			},
+	// 		}
+	// 	},
+	// },
 
-	{
-		displayName: 'Filter',
-		name: 'filter',
-		type: 'json',
-		placeholder: 'Enter filter JSON. E.g.: {"name": "name", "operator": "=", "value": "John"} or {"operator": "and", "filters": [ {"name": "name", "operator": "=", "value": "John"}, {"name": "age", "operator": ">", "value": "30"} ] }',
-		default: '{}',
-		displayOptions: {
-			show: {
-				operation: ['filterDataset'],
-			},
-		},
-		description: 'JSON filter. Supports a simple filter or a composite filter (using "and" with filters array).',
-		routing: {
-			send: {
-				type: 'body',
-				property: 'filter'
-			},
-		},
-	},
+	// {
+	// 	displayName: 'Filter',
+	// 	name: 'filter',
+	// 	type: 'json',
+	// 	placeholder: 'Enter filter JSON. E.g.: {"name": "name", "operator": "=", "value": "John"} or {"operator": "and", "filters": [ {"name": "name", "operator": "=", "value": "John"}, {"name": "age", "operator": ">", "value": "30"} ] }',
+	// 	default: '{}',
+	// 	displayOptions: {
+	// 		show: {
+	// 			operation: ['filterDataset'],
+	// 		},
+	// 	},
+	// 	description: 'JSON filter. Supports a simple filter or a composite filter (using "and" with filters array).',
+	// 	routing: {
+	// 		send: {
+	// 			type: 'body',
+	// 			property: 'filter'
+	// 		},
+	// 	},
+	// },
 	// {
 	// 	displayName: 'Filter Type',
 	// 	name: 'filter_type',
